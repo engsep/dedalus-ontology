9d11985bea43d54942dacb8a2322868165bee602
engsep <smartenergyprojects.eng@gmail.com>
2025-01-14 14:54:44 +0000

    Nginx and ontology refined

--- list.sh
+++ list.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-docker ps -a --format "table {{.Names}}\t{{.Status}}\t{{.Ports}}\t{{.ID}}"
+docker ps -a --format "table {{.Names}}\t{{.Status}}\t{{.Ports}}"
 
 read enter
-docker ps -a --format "table {{.Networks}}\t{{.Names}}\t{{.RunningFor}}\t{{.Image}}\t{{.Command}}\t{{.Mounts}}"
+docker ps -a --format "table {{.Networks}}\t{{.Names}}\t{{.RunningFor}}\t{{.Image}}\t{{.Command}}\t{{.Mounts}}\t{{.ID}}"
 
 read enter
 docker compose logs -n 10 -f
